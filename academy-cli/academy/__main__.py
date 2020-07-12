5932d2fd6d2c0b251b0487225b14301e2f1202c2
nduwal <duwalniru0@gmail.com>
2020-07-13 00:11:37 +0545

    update read delete operation for student

--- academy-cli/academy/__main__.py
+++ academy-cli/academy/__main__.py
@@ -3,19 +3,84 @@ from .course import Course
 from .student import Student
 
 def main():
-    print('in main')
-    args = sys.argv[1:]
-    print('count of args::{}'.format(len(args)))
     print("welcomme to IT Academy")
-    options = {1:"inquiry"
-                , 2: "Enroll"
-                , 3:"Update"
-                , 4: "Delete"}
-    optionII= {1:"student",2:"course"}
-    for arg in args:
-        print('passed argument::{}'.format(arg))
     studentObj = Student()
-    courseObj = Course()
+    # courseObj = Course()
+    choice = ''
+    sub_choice = ''
+    while choice != 'q': 
+        choice = get_user_choice()
+
+        # Respond to the user's choice.
+        if choice == '1':
+            sub_choice = get_student_options()
+        elif choice == '2':
+            sub_choice = get_course_options()
+        elif choice == 'q':
+            print("\n Bye Bye.")
+        else:
+            print("\nI didn't understand that choice.\n")
+
+        if choice == '1' and sub_choice =='1':            
+            studentObj.get_all_students(mode='r')
+        elif choice == '1' and sub_choice =='2':
+            name = input("enter your fullname:::eg(John Doe)\n")
+            studentObj.get_Student_info(format_name(name), mode='r')
+        elif choice == '1' and sub_choice =='3':
+            name = input("enter your fullname:::eg(John Doe)\n")
+            studentObj.update_student_detail(format_name(name), mode='a'), 
+        elif choice == '1' and sub_choice =='4':
+            name = input("enter your fullname:::eg(John Doe)\n")
+            email = input("enter your email:::eg(John@mail.com)\n")
+            course = input("enter Courses to enroll:::eg(python)\n")
+            balance = input("make full installment :::eg(1000000)\n")
+            data = []
+            data.append(name)
+            data.append(email)
+            data.append(balance)
+            studentObj.create_new_student(data, mode='a+')
+        elif choice == '1' and sub_choice =='5':
+            name = input("enter your fullname:::eg(John Doe)\n")
+            studentObj.delete_student(format_name(name), mode='r')
+        elif choice == 'q':
+            print("\n Bye Bye.")
+        else:
+            print("\nI didn't understand that choice.\n")
+
+    # args = sys.argv[1:]
+    # print('count of args::{}'.format(len(args)))
+    
+    # for arg in args:
+    #     print('passed argument::{}'.format(arg))    
+
+def get_user_choice():
+    # Let users know what they can do.
+    print("\n[1] Enter into Student Module.")
+    print("[2] Enter Course Module.")
+    print("[q] Quit.")
+    return input("What would you like to do? ")
+
+def get_student_options():
+    print("\n[1] get all Students information")
+    print("[2] view your Students profile")
+    print("[3] Update your profile.")
+    print("[4] Register into course")
+    print("[5] Delete your Courses.")
+    print("[q] Quit.")
+    return input("What would you like to do? ")
+
+def get_course_options():
+    print("\n[1] Enquire about Course")
+    print("[2] Enroll into a course.")
+    print("[q] Quit.")    
+    return input("What would you like to do? ")
+
+def format_name(name):
+    fullname = name.split(' ')
+    for i in range(len(fullname)):
+        fullname[i] = fullname[i].capitalize()
+    formatted_string =  ' '.join(fullname)
+    return formatted_string
     
 
 if __name__ == '__main__':
--- academy-cli/academy/student.py
+++ academy-cli/academy/student.py
@@ -1,41 +1,102 @@
 import csv
 
 
-class Student:
-
-    
-    def __init__(self, mode):
-        this.mode = mode
+class Student:    
+    def __init__(self):
+        # this.mode = mode
         # this.data = data
         pass
 
-    def file_read_write(self, mode, data=None, id=None):
-        # if mode = 'r':
-        # else: mode = 'a'
-        with open ('files/students.csv', mode) as data:
-            students =  csv.reader(data)
-            if data is not None:
-                print(data)
-                # students.write(data)
-            if id is not None:
-                print(id)
+    def file_read_write(self, mode, data=None):
+        with open ('files/students.csv', mode) as student_data:
+            
+            result=[]
+            if mode == 'r':
+                students =  csv.DictReader(student_data)
+                if students is not None:
+                    for row in students:
+                        result.append(row)
+            else:
+                # Create a writer object from csv module
+                students =  csv.writer(student_data)
+                # Add contents of list as last row in the csv file
+                students.writerow(data)
+        return result
 
 
-    def get_all_students(self):
+    def get_all_students(self, mode):
+        students_list = self.file_read_write(mode)
+        val = next(iter(students_list))
+        print(list(val.keys()))
+        # header=[]
+        # for key in val.keys():           
+        #     header.append(key)
+        # ', '.join(header)
+        # print(header)
+        for item in students_list:
+            print(list(item.values()))
+            # result = list(map(lambda n: n.values(), item))
+            # print(result)
+            # for student in item.values():
+            #     result.append(student)
+            # print(', '.join(result))
+            
+            
+        # print(students_list)
         
-            students =  csv.reader(data)
-            print("The list of students are::\nS")
-            for rows in students:
-                print("{} : {}".format(rows[0], rows[1]))
-            print("\n")
+        
+            # students =  csv.reader(student_data)
+            # print("The list of students are::\nS")
+            # for rows in students:
+            #     print("{} : {}".format(rows[0], rows[1]))
+            # print("\n")
 
-    def get_Student_info(self, id):
+
+    def get_Student_info(self, name, mode):
+        students_list = self.file_read_write(mode)
+        for item in students_list:
+            if name in item.values():
+                print(item)
+            else:
+                print("no record found")
+                break
+
+    def update_student_detail(self, id, mode):
+        students_list = self.file_read_write(mode)
+        print("update_student_detail")
+        print(mode)
+
+    def create_new_student(self, student_data, mode):
+        print(student_data)
+        students_list = self.file_read_write(mode, data = student_data)
+        print(self.__get_records_count(students_list))
+        
+        print("update_student_detail")
+        print(mode)
+        
         pass
 
-    def update_student_detail(self, id):
-        pass
+    def delete_student(self, name, mode):
+        updated_list =[]
+        students_list = self.file_read_write(mode)
+        for student in students_list:
+            if student.get('student_name') != name:
+                updated_list.append(student)
+        self.__update_file(updated_list)
+    
+    def __update_file(self, updatedlist):
+        print(updatedlist)
+        val = next(iter(updatedlist))
+        header = list(val.keys())
+        print(header)
+        with open('files/students.csv',"w") as f:
+            Writer=csv.DictWriter(f, fieldnames=header)
+            Writer.writeheader()
+            for row in updatedlist:
+                Writer.writerow(row)
+            print("File has been updated")
 
-    def delete_student(self, id):
-        pass
+    def __get_records_count(self, student_list):
+        return student_list[-1:]
 
     
